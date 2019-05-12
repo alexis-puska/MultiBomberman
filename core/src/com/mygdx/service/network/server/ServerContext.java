7639d757b930723e2c7b37829f10c3c4a8153500
apuskarczyk <apuskarczyk@PORT-APU.IOCEANNT.FR>
2019-05-12 17:28:57 +0200

    update

--- core/src/com/mygdx/service/network/server/ServerContext.java
+++ core/src/com/mygdx/service/network/server/ServerContext.java
@@ -1,16 +1,5 @@
 package com.mygdx.service.network.server;
 
-import com.mygdx.domain.Player;
-import com.mygdx.domain.game.Bombe;
-import com.mygdx.domain.game.Brick;
-import com.mygdx.domain.game.Fire;
-import com.mygdx.domain.level.Hole;
-import com.mygdx.domain.level.Interrupter;
-import com.mygdx.domain.level.Mine;
-import com.mygdx.domain.level.Rail;
-import com.mygdx.domain.level.SuddenDeathWall;
-import com.mygdx.domain.level.Teleporter;
-import com.mygdx.domain.level.Trolley;
 import com.mygdx.enumeration.ServerStateEnum;
 import com.mygdx.service.network.enumeration.NetworkGameRequestEnum;
 
@@ -82,65 +71,5 @@ public class ServerContext {
 
 	public static void addTime(float time) {
 		NetworkGameRequestEnum.TIME.ordinal();
-	}
-
-	public static void addPlayer(Player player) {
-
-	}
-
-	public static void addBombe(Bombe bombe) {
-
-	}
-
-	public static void addBrick(Brick brick) {
-
-	}
-
-	public static void addRail(Rail rail) {
-
-	}
-
-	public static void addFire(Fire fire) {
-
-	}
-
-	public static void addMine(Mine mine) {
-
-	}
-
-	public static void addHole(Hole hole) {
-
-	}
-
-	public static void addInterrupteur(Interrupter interrupteur) {
-
-	}
-
-	public static void addSuddenDeathWall(SuddenDeathWall sdWall) {
-
-	}
-
-	public static void addTeleporter(Teleporter teleporter) {
-
-	}
-
-	public static void addTrolley(Trolley trolley) {
-
-	}
-
-	public static void addScore(Player player) {
-
-	}
-
-	public static void addPause() {
-
-	}
-
-	public static void addMenu() {
-
-	}
-
-	public static void addLight(float x, float y, float radius) {
-
 	}
 }
